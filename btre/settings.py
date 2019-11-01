af4338325a9b741a7f047738049218d8384d6183
Moazam05 <54484469+Moazam05@users.noreply.github.com>
2019-11-01 17:07:46 +0500

    Update settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '*!#w10=q@g&l^wa5xfwh+_7!utx!zw!m62q_srg6&$t7a84szg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
